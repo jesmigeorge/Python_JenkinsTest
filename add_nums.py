6e6592584a397a3fdbf8ea4dde0039488c2a106b
Jesmi-04 <jesmigeorge04@gmail.com>
2025-10-02 10:37:57 +0530

    Inputs via pass args

--- add_nums.py
+++ add_nums.py
@@ -1,4 +1,5 @@
-a = int(input("Enter 1st num : "))
-b = int(input("Enter 2nd num : "))
+import sys
+a = int(sys.argv[1])
+b = int(sys.argv[2])
 c = a + b
 print(f"The sum of nums {a} and {b} is : {c}")
