0927aa1472f949a4b840649178242c3f9d9dea81
Jesmi-04 <jesmigeorge04@gmail.com>
2025-10-02 10:52:33 +0530

    Added multiplication

--- add_nums.py
+++ add_nums.py
@@ -3,5 +3,7 @@ a = int(sys.argv[1])
 b = int(sys.argv[2])
 c = a + b
 diff = b - a
+prod = b * a
 print(f"The sum of nums {a} and {b} is : {c}")
 print(f'The diff of numd {b} and {a} is : {diff}')
+print(f'The product of 2 nums {a} and {b} is : {prod}')
